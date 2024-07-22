b7eec52929860182fca53df39648abbefcf037cc
Naveen U <142392636+Naveen00222@users.noreply.github.com>
2024-07-22 16:59:49 +0530

    Add files via upload

---
+++ javascript/javascript.js
@@ -0,0 +1,16 @@
+function correct(){
+    document.getElementsByClassName("true")[0].style.backgroundColor="green";
+    document.getElementsByClassName("opt3")[0].style.backgroundColor="green";
+}
+function wrong1(){
+    document.getElementsByClassName("main1")[0].style.backgroundColor="red";
+    document.getElementsByClassName("opt1")[0].style.backgroundColor="red";
+}
+function wrong2(){
+    document.getElementsByClassName("main2")[0].style.backgroundColor="red";
+    document.getElementsByClassName("opt2")[0].style.backgroundColor="red";
+}
+function wrong3(){
+    document.getElementsByClassName("main3")[0].style.backgroundColor="red";
+    document.getElementsByClassName("opt0")[0].style.backgroundColor="red";
+}
